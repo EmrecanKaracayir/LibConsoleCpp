8e4a6ba690c5989eda2592a2ccfabd2cfb90cd28
EmrecanKaracayir <developer@karacayir.com>
2024-08-19 22:54:55 +0300

    Add project files.

---
+++ projects/LibConsole++/source/pch.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+// NOLINTBEGIN(misc-include-cleaner)
+
+// ----------------------------------------< Foundation >---------------------------------------- //
+
+// fn headers
+#include <Foundation/concepts.hpp>
+#include <Foundation/constants.hpp>
+#include <Foundation/containers.hpp>
+#include <Foundation/errors.hpp>
+#include <Foundation/types.hpp>
+#include <Foundation/utilities.hpp>
+
+// fn::Support headers
+#include <Foundation/Support/consteval.ipp>
+#include <Foundation/Support/narrow.ipp>
+
+// fn::Utility headers
+#include <Foundation/Utility/log.ipp>
+#include <Foundation/Utility/what.ipp>
+
+// NOLINTEND(misc-include-cleaner)
